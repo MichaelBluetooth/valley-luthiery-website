dba9bac4de5032c419d5d981af78b5170b37984a
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2025-03-23 19:56:43 -0400

    Optimize image sizes, favicon, footer component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavComponent } from '../nav/nav.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, NavComponent],
+  imports: [RouterModule, NavComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
